526609d3dd32c7d3fee27075cebbd428da2dc693
MindulaHewadewage <mindufernando@gmail.com>
2023-02-08 16:38:47 +0100

    struttura html css

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
-<body>
-    
+
+<body class="bg-primary">
+    <header class="container mt-4 text-center text-light">
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+    </header>
+
+    <main class="small-container mt-5 ">
+        <p>Lunghezza Password</p>
+        <form action="password_generator">
+            <input type="text">
+            <button class="btn btn-primary">Invia</button>
+            <button class="btn btn-secondary">annulla</button>
+        </form>
+    </main>
+
 </body>
+
 </html>
